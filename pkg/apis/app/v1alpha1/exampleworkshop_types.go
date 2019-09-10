6c3ee071b61b55c74c123fff77cdfdb9a881148c
Michał Pawłowski <michal.pawlowski@container-solutions.com>
2019-09-10 16:48:06 +0200

    Add solution for second exercise

---
+++ pkg/apis/app/v1alpha1/exampleworkshop_types.go
@@ -0,0 +1,60 @@
+package v1alpha1
+
+import (
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
+// NOTE: json tags are required.  Any new fields you add must have json tags for the fields to be serialized.
+
+// ExampleWorkshopSpec defines the desired state of ExampleWorkshop
+// +k8s:openapi-gen=true
+type ExampleWorkshopSpec struct {
+	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+	Size int32 `json:"size"`
+	RedisContainer  RedisContainer  `json:"redisContainer,required"`
+}
+
+type RedisContainer struct {
+	Name 	 		  string 	`json:"name,required"`
+	Image 	 		  string 	`json:"image,required"`
+	Command  		  []string  `json:"command,required"`
+	ContainerPort 	  int32 	`json:"containerPort",required`
+	ContainerPortName string    `json:"containerPortName,required"`
+}
+
+// ExampleWorkshopStatus defines the observed state of ExampleWorkshop
+// +k8s:openapi-gen=true
+type ExampleWorkshopStatus struct {
+	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
+	// Important: Run "operator-sdk generate k8s" to regenerate code after modifying this file
+	// Add custom validation using kubebuilder tags: https://book-v1.book.kubebuilder.io/beyond_basics/generating_crd.html
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ExampleWorkshop is the Schema for the exampleworkshops API
+// +k8s:openapi-gen=true
+// +kubebuilder:subresource:status
+type ExampleWorkshop struct {
+	metav1.TypeMeta   `json:",inline"`
+	metav1.ObjectMeta `json:"metadata,omitempty"`
+
+	Spec   ExampleWorkshopSpec   `json:"spec,omitempty"`
+	Status ExampleWorkshopStatus `json:"status,omitempty"`
+}
+
+// +k8s:deepcopy-gen:interfaces=k8s.io/apimachinery/pkg/runtime.Object
+
+// ExampleWorkshopList contains a list of ExampleWorkshop
+type ExampleWorkshopList struct {
+	metav1.TypeMeta `json:",inline"`
+	metav1.ListMeta `json:"metadata,omitempty"`
+	Items           []ExampleWorkshop `json:"items"`
+}
+
+func init() {
+	SchemeBuilder.Register(&ExampleWorkshop{}, &ExampleWorkshopList{})
+}
---
+++ pkg/controller/exampleworkshop/exampleworkshop_controller.go
@@ -0,0 +1,180 @@
+package exampleworkshop
+
+import (
+	"context"
+
+	appsv1 "k8s.io/api/apps/v1"
+	appv1alpha1 "github.com/ContainerSolutions/example-operator/pkg/apis/app/v1alpha1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/controller"
+	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
+	"sigs.k8s.io/controller-runtime/pkg/handler"
+	"sigs.k8s.io/controller-runtime/pkg/manager"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+	"sigs.k8s.io/controller-runtime/pkg/source"
+)
+
+var log = logf.Log.WithName("controller_exampleworkshop")
+
+/**
+* USER ACTION REQUIRED: This is a scaffold file intended for the user to modify with their own Controller
+* business logic.  Delete these comments after modifying this file.*
+ */
+
+// Add creates a new ExampleWorkshop Controller and adds it to the Manager. The Manager will set fields on the Controller
+// and Start it when the Manager is Started.
+func Add(mgr manager.Manager) error {
+	return add(mgr, newReconciler(mgr))
+}
+
+// newReconciler returns a new reconcile.Reconciler
+func newReconciler(mgr manager.Manager) reconcile.Reconciler {
+	return &ReconcileExampleWorkshop{client: mgr.GetClient(), scheme: mgr.GetScheme()}
+}
+
+// add adds a new Controller to mgr with r as the reconcile.Reconciler
+func add(mgr manager.Manager, r reconcile.Reconciler) error {
+	// Create a new controller
+	c, err := controller.New("exampleworkshop-controller", mgr, controller.Options{Reconciler: r})
+	if err != nil {
+		return err
+	}
+
+	// Watch for changes to primary resource ExampleWorkshop
+	err = c.Watch(&source.Kind{Type: &appv1alpha1.ExampleWorkshop{}}, &handler.EnqueueRequestForObject{})
+	if err != nil {
+		return err
+	}
+
+	// TODO(user): Modify this to be the types you create that are owned by the primary resource
+	// Watch for changes to secondary resource Pods and requeue the owner ExampleWorkshop
+	err = c.Watch(&source.Kind{Type: &appsv1.Deployment{}}, &handler.EnqueueRequestForOwner{
+		IsController: true,
+		OwnerType:    &appv1alpha1.ExampleWorkshop{},
+	})
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+// blank assignment to verify that ReconcileExampleWorkshop implements reconcile.Reconciler
+var _ reconcile.Reconciler = &ReconcileExampleWorkshop{}
+
+// ReconcileExampleWorkshop reconciles a ExampleWorkshop object
+type ReconcileExampleWorkshop struct {
+	// This client, initialized using mgr.Client() above, is a split client
+	// that reads objects from the cache and writes to the apiserver
+	client client.Client
+	scheme *runtime.Scheme
+}
+
+// Reconcile reads that state of the cluster for a ExampleWorkshop object and makes changes based on the state read
+// and what is in the ExampleWorkshop.Spec
+// The Controller will requeue the Request to be processed again if the returned error is non-nil or
+// Result.Requeue is true, otherwise upon completion it will remove the work from the queue.
+func (r *ReconcileExampleWorkshop) Reconcile(request reconcile.Request) (reconcile.Result, error) {
+	reqLogger := log.WithValues("Request.Namespace", request.Namespace, "Request.Name", request.Name)
+	reqLogger.Info("Reconciling Redis")
+
+	// Fetch the Memcached instance
+	redis := &appv1alpha1.ExampleWorkshop{}
+	err := r.client.Get(context.TODO(), request.NamespacedName, redis)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			reqLogger.Info("Redis resource not found. Ignoring since object must be deleted")
+			return reconcile.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		reqLogger.Error(err, "Failed to get Redis")
+		return reconcile.Result{}, err
+	}
+
+	// Check if the deployment already exists, if not create a new one
+	found := &appsv1.Deployment{}
+	err = r.client.Get(context.TODO(), types.NamespacedName{Name: redis.Name, Namespace: redis.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		dep := r.deploymentForRedis(redis)
+		reqLogger.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.client.Create(context.TODO(), dep)
+		if err != nil {
+			reqLogger.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return reconcile.Result{}, err
+		}
+		// Deployment created successfully - return and requeue
+		return reconcile.Result{Requeue: true}, nil
+	} else if err != nil {
+		reqLogger.Error(err, "Failed to get Deployment")
+		return reconcile.Result{}, err
+	}
+
+	// Ensure the deployment size is the same as the spec
+	size := redis.Spec.Size
+	if *found.Spec.Replicas != size {
+		found.Spec.Replicas = &size
+		err = r.client.Update(context.TODO(), found)
+		if err != nil {
+			reqLogger.Error(err, "Failed to update Deployment", "Deployment.Namespace", found.Namespace, "Deployment.Name", found.Name)
+			return reconcile.Result{}, err
+		}
+		// Spec updated - return and requeue
+		return reconcile.Result{Requeue: true}, nil
+	}
+
+	return reconcile.Result{}, nil
+}
+
+// deploymentForRedis returns a redis Deployment object
+func (r *ReconcileExampleWorkshop) deploymentForRedis(m *appv1alpha1.ExampleWorkshop) *appsv1.Deployment {
+	ls := labelsForRedis(m.Name)
+	replicas := m.Spec.Size
+
+	deployment := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{{
+						Image:   m.Spec.RedisContainer.Image,
+						Name:    m.Spec.RedisContainer.Name,
+						Command: m.Spec.RedisContainer.Command,
+						Ports: []corev1.ContainerPort{{
+							ContainerPort: m.Spec.RedisContainer.ContainerPort,
+							Name:          m.Spec.RedisContainer.ContainerPortName,
+						}},
+					}},
+				},
+			},
+		},
+	}
+	// Set Redis instance as the owner and controller
+	controllerutil.SetControllerReference(m, deployment, r.scheme)
+	return deployment
+}
+
+// labelsForRedis returns the labels for selecting the resources
+// belonging to the given memcached CR name.
+func labelsForRedis(name string) map[string]string {
+	return map[string]string{"app": "redis", "redis_cr": name}
+}
